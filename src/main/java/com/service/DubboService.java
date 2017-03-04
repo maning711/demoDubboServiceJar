bc17fb5ef8420e9a93a50008c64790e6f9610589
maning711 <277943578@qq.com>
2017-03-04 20:39:06 +0800

    Initial commit

---
+++ src/main/java/com/service/DubboService.java
@@ -0,0 +1,15 @@
+package com.service;
+
+import java.util.List;
+
+/**
+ * Created by maning  on 2017/3/4.
+ */
+public interface DubboService {
+
+    /**
+     * 测试用dubbo接口
+     * @return
+     */
+    List<String> testGetData();
+}
